8fa1cba3fdb67499c687b2d8cadd9d9a5249a69a
Mike Burnside <mburnside+git@salesforce.com>
2022-06-08 15:41:28 +1000

    test

--- force-app/main/default/lwc/mbtestLWC/mbtestLWC.js
+++ force-app/main/default/lwc/mbtestLWC/mbtestLWC.js
@@ -1,3 +1,16 @@
 import { LightningElement } from 'lwc';
 
-export default class MbtestLWC extends LightningElement {}
+export default class MbtestLWC extends LightningElement {
+    handleClick(event) {
+        const logger = this.template.querySelector('c-logger');
+        logger.error('Hello, world!').addTag('some important tag');
+        logger.warn('Hello, world!');
+        logger.info('Hello, world!');
+        logger.debug('Hello, world!');
+        logger.fine('Hello, world!');
+        logger.finer('Hello, world!');
+        logger.finest('Hello, world!');
+        logger.saveLog();
+        this.clickedButtonLabel = event.target.label;
+    }
+}
--- orgInit.sh
+++ orgInit.sh
@@ -1,13 +1,16 @@
 #Mikes script to set up the dreamhouselwc org
 
 # 1. delete scrtatch org
-sfdx force:org:delete -u dreamhouse
+sfdx force:org:delete -u dreamhousescratch
 
 #2. create scratch org
-sfdx shane:org:create --userprefix dhuser -o my.demo -s -d 30 -a dreamhouse
+sfdx shane:org:create --userprefix dhuser -o my.demo -s -d 30 -a dreamhousescratch
 
 #... and set the password
 sfdx shane:user:password:set -p sfdx1234 -g User -l User
+
+# install nebula logging 
+sfdx force:package:install -w 20 -r -p  04t5Y0000015lXSQAY
 
 #3. push the source
 sfdx force:source:push
@@ -20,13 +23,16 @@ sfdx force:user:permset:assign -n dreamhouse
 sfdx force:data:tree:import -p data/sample-data-plan.json
 
 #6. install stre
-sfdx force:package:install -w 20 -r -p 04t1t000003DLAQAA4
+sfdx force:package:install -w 20 -r -p 04t1t000003Po3VAAS
 
 #7. install streaming channels and push topics
-//sfdx force:apex:execute -f ./apex/setup.apsfdx force:user:create --setalias qa-user --definitionfile config/user-def.json
-//sfdx shane:user:password:set -p sfdx1234 -g Prop -l Manager
+#sfdx force:apex:execute -f ./apex/setup.ap
+#sfdx force:user:create --setalias qa-user --definitionfile config/user-def.json
+#sfdx shane:user:password:set -p sfdx1234 -g Prop -l Manager
 
-#8. open the org
+
+
+#9. open the org
 sfdx force:org:open
 
 # all done should be working now and I think codecov may work
